2aebe8c1910e6740844bdd6a7e7ae68d5143d0bc
Neakit <pavliukny@gmail.com>
2020-04-27 16:31:58 +0300

    add vue and auth scaffolding

--- routes/admin.php
+++ routes/admin.php
@@ -4,19 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Admin Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register admin routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group.
+| contains the "admin" middleware group.
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
