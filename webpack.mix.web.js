9ad51b98c9d7cc66775ab56cff1fa3020b024b02
Neakit <pavliukny@gmail.com>
2020-04-29 15:27:42 +0300

    change webpack mix config for web

--- webpack.mix.web.js
+++ webpack.mix.web.js
@@ -1,9 +1,24 @@
-const mix = require('laravel-mix');
+const mix                 = require("laravel-mix");
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+
+mix.setPublicPath('public/web/');
+
+Mix.listen('configReady', config => {
+    const scssRule = config.module.rules.find(r => r.test.toString() === /\.scss$/.toString());
+    const scssOptions = scssRule.loaders.find(l => l.loader === 'sass-loader').options;
+    scssOptions.data = '@import "./resources/web/sass/app.scss"';
+
+    const sassRule = config.module.rules.find(r => r.test.toString() === /\.sass$/.toString());
+    const sassOptions = sassRule.loaders.find(l => l.loader === 'sass-loader').options;
+    sassOptions.data = '@import "./resources/web/sass/app.scss"';
+});
 
 mix.options({
     extractVueStyles: true,
     processCssUrls: false
 })
-    .setPublicPath('public/web/')
+    .webpackConfig({
+        plugins: [new VuetifyLoaderPlugin()]
+    })
     .js('resources/web/js/entry.js', 'js')
     .sass('resources/web/sass/app.scss', 'css');
