9f62eeaeb5edc5fd45893fb64f4889ab1cc59b21
Neakit <pavliukny@gmail.com>
2020-04-29 16:45:15 +0300

    add todo list to home page

--- resources/admin/js/pages/Dashboard.vue
+++ resources/admin/js/pages/Dashboard.vue
@@ -1,5 +1,34 @@
 <template>
-    <div>
-        dashboard
-    </div>
+    <v-card
+        tile
+        class="ma-3"
+    >
+        <v-list-item>
+            <v-list-item-content>
+                <v-list-item-title>TODO:</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-content>
+                <v-list-item-title>- {{ item }}</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+    </v-card>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                items: [
+                    'DRY у компонентов pages - вынести в рутовый компонент от которого екстендиться',
+                    'вынести запросы данных в рутовый компонент',
+                    'Валидация полей, чтобы не вылезала ошибки моделей',
+                    'удалить bootstrap.js',
+                    'удалить неиспользуемые пакеты'
+                ]
+            }
+        }
+    }
+</script>
