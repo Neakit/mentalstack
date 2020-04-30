7811e476e5c401e6734c867123a01039e8d4c591
Neakit <pavliukny@gmail.com>
2020-04-30 13:41:12 +0300

    create static templates

---
+++ resources/web/js/pages/Contacts.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container py-2">
+        <h1>Телефоны отдела продаж:</h1>
+        <ul>
+            <li>
+                <a class="phone-title" href="tel:+74955065236">+7 (495) 506-52-36</a>
+            </li>
+            <li>
+                <a class="phone-title" href="tel:+79265908657">+7 (926) 590-86-57</a>
+            </li>
+        </ul>
+        <p>
+            Мы будем рады помочь вам (и вашему грузовику :) при выборе
+            необходимой запчасти, а также готовы ответить вам на любые вопросы
+            касательно запасных частей на европейские грузовики любых модификаций
+            указанных марок.
+        </p>
+
+        <p>
+            Электронная почта: <strong><a href="mailto:info@auto-cosmos.ru">info@auto-cosmos.ru</a></strong><br>
+            Icq для заказов и вопросов: <strong>325 180 346</strong><br>
+            Skype: <strong>auto-cosmos.ru</strong></p>
+        <p>Адрес: <strong>Москва, Ленинградское шоссе, д. 236</strong></p>
+    </div>
+</template>
