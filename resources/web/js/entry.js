883353d57b6df72cdafcfc8e49fd87ad3711e22f
Neakit <pavliukny@gmail.com>
2020-04-30 13:46:59 +0300

    change entry point.js + app.vue

--- resources/web/js/entry.js
+++ resources/web/js/entry.js
@@ -1,37 +1,31 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue     from 'vue';
+import App     from "./App";
+import router  from "./router";
+import Vuetify from "vuetify/lib";
+import store   from "./store";
 
-require('./bootstrap');
+Vue.use(Vuetify);
 
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-import App from './App'
-import router from './router';
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 new Vue({
     el: '#app',
     router,
+    store,
+    vuetify: new Vuetify({
+        theme: {
+            dark: false
+        }
+    }),
     components: {
         App
     }
