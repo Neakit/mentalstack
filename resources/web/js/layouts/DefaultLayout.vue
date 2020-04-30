70f5559272cfa6cacb718ece2eae0b8562273e69
Neakit <pavliukny@gmail.com>
2020-04-30 13:43:21 +0300

    create default web side layout

--- resources/web/js/layouts/DefaultLayout.vue
+++ resources/web/js/layouts/DefaultLayout.vue
@@ -1,6 +1,42 @@
 <template>
     <div>
+        <v-toolbar color="orange darken-1" dark>
+            <v-container>
+                <v-row no-gutters>
+                    <v-col cols="1" class="d-flex align-center">
+                        <v-btn icon color="white">
+                            <v-icon>menu</v-icon>
+                        </v-btn>
+                    </v-col>
+                    <v-col class="text-center">
+                        <v-btn height="60" exact to="/" text>Главная</v-btn>
+                        <v-btn height="60" exact to="/shop" text>Магазин</v-btn>
+                        <v-btn height="60" exact to="/delivery" text>Доставка</v-btn>
+                        <v-btn height="60" exact to="/guarantee" text>Гарантия</v-btn>
+                        <v-btn height="60" exact to="/contacts" text>Контакты</v-btn>
+                    </v-col>
+                    <v-col cols="1" class="d-flex align-center">
+                        <v-btn icon color="white">
+                            <v-icon>email</v-icon>
+                        </v-btn>
+                        <v-btn icon>
+                            <v-icon color="white">phone</v-icon>
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-toolbar>
         <router-view/>
-        this will be default web layout
+
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
